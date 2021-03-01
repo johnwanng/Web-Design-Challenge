38aa7721535b624256ca0834f0f3231ab709cb01
John Wan Ng <johnwanng@Johns-MBP.lan>
2021-03-01 21:29:46 +0800

    commit

---
+++ panda.py
@@ -0,0 +1,14 @@
+# import DataFrame 
+import pandas as pd 
+  
+# using DataFrame.to_html() method 
+city_data_to_load = "Resources/cities.csv"
+
+# Read City Data File and store into Pandas DataFrames
+city_data = pd.read_csv(city_data_to_load)
+html = city_data.to_html()
+
+#write html to file
+text_file = open("raw_data.html", "w")
+text_file.write(html)
+text_file.close()
