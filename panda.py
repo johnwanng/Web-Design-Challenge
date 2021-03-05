4de5b3d6147f1f5e9dff6ac28e705f92233d3bfe
John Wan Ng <johnwanng@Johns-MBP.lan>
2021-03-06 00:12:14 +0800

    commit

--- panda.py
+++ panda.py
@@ -6,7 +6,7 @@ city_data_to_load = "Resources/cities.csv"
 
 # Read City Data File and store into Pandas DataFrames
 city_data = pd.read_csv(city_data_to_load)
-html = city_data.to_html()
+html = city_data.to_html(index = False)
 
 #write html to file
 text_file = open("raw_data.html", "w")
